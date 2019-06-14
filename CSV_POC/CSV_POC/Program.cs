5db76d23871f68ca1d60cfaba9a3da08fac6a722
corso-enaip-2019 <prog17-corso-enaip-2019@users.noreply.github.com>
2019-06-14 12:34:49 +0200

    Added Proof of Concept: CSV Conversion

---
+++ CSV_POC/CSV_POC/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_POC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = "people.csv";
+
+            List<Person> people = new List<Person>
+            {
+                new Person
+                {
+                    Id = 1,
+                    Name = "Mario",
+                    Surname = "Rossi",
+                    Email = "mario.rossi@yourmail.com"
+                },
+                new Person
+                {
+                    Id = 2,
+                    Name = "Luigi",
+                    Surname = "Verdi",
+                    Email = "luigi.verdi@yourmail.com"
+                }
+            };
+
+            IEnumerable<string> strings = people.Select(p => $"{p.Id},{p.Name},{p.Surname},{p.Email}");
+            
+            File.WriteAllLines(filepath, strings); // AppendAllLines per non sostituire
+        }
+    }
+}
